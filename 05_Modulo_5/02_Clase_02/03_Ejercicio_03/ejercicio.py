2f1dec332f0ebaa9550d5bb25b535f1f613414bb
Masterdevsenior <lgmoleros@gmail.com>
2025-08-17 19:22:11 -0500

    commit

--- 05_Modulo_5/02_Clase_02/03_Ejercicio_03/ejercicio.py
+++ 05_Modulo_5/02_Clase_02/03_Ejercicio_03/ejercicio.py
@@ -122,4 +122,19 @@ if __name__ == "__main__":
                 print(f"Epoch [{epoch}/{EPOCHS}] Step [{i+1}/{len(loader)}] "
                     f"Loss_D: {loss_D.item():.4f} Loss_G: {loss_G.item():.4f}"
                     )
+    with torch.no_grad():
+        samples = G(fixed_noise).cpu()
+        grid = make_grid(samples, nrow=6, normalize=True, value_range=(-1, 1))
+        save_image(grid, os.path.join(OUT_DIR, f"epoch_{epoch:02d}.png"))        
         
+    torch.save(G.state_dict(), os.path.join(OUT_DIR, f"G_epoch_{epoch}.pth"))
+    torch.save(D.state_dict(), os.path.join(OUT_DIR, f"D_epoch_{epoch}.pts"))
+
+    print("Entrenamiento finalizado. Imágenes y chekpoints en: ", OUT_DIR)
+
+    img = plt.imread(os.path.join(OUT_DIR, f"epoch_{EPOCHS:02d}.png"))
+    plt.figure(figsize=(6, 6))
+    plt.imshow(img)
+    plt.axis('off')
+    plt.title(f"Ejemplo despues de la epoca {EPOCHS}")
+    plt.show()
