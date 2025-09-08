4e6dd7377f3c709c821ce7276ab5c1e3e365a648
Masterdevsenior <lgmoleros@gmail.com>
2025-09-07 21:37:27 -0500

    commit

--- 06_Modulo_6/ejercicio_tres/main.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, when, count, length, regexp_replace, upper, trim
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType
-import requests
-
